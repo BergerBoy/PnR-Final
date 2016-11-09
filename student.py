545c1c5f49e5919cea4fda57d56493b10c9aa973
bergerc <bergerc@gilmour.org>
2016-11-09 09:31:20 -0500

    error change for self.encF(10)

--- student.py
+++ student.py
@@ -73,7 +73,7 @@ class GoPiggy(pigo.Pigo):
         #TODO: If while loop fails, check for other paths
         while True:
             while self.isClear():
-                encF(10)
+                self.encF(10)
             answer = self.choosePath()
             if answer == "left":
                 self.encL(5)
